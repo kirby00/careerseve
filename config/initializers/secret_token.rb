036b3e2e5758d917eae82ca6979d404f4ab6c533
kirby00 <mbkirby@gmail.com>
2013-03-18 14:14:37 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Careerseve::Application.config.secret_token = '25529cd4ccb10296fc0dc3dd05d90b395616678d06089f5364ce2d2f9f4d651efac6a1441c1d5a216dcfd8d8a057a545fdb8f175d5384eabbde9054acdecb1cc'
